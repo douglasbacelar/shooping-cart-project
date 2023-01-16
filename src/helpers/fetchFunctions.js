800372128df1dfc4e985fadb31d53fdadea7a504
Douglas Bacelar <bacelardouglas@outlook.com>
2023-01-16 18:36:37 -0300

    Requisitos 1 a 3

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (element) => {
+  try {
+    if (!element) throw new Error('Termo de busca não informado');
+    const API = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${element}`);
+    const data = await API.json();
+    return data.results;
+  } catch (error) {
+    return error;
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-const x = 0;
+const getSection = document.querySelector('.products');
+
+const getProducts = async () => {
+  const product = await fetchProductsList('computador');
+  product.forEach((element) => {
+    const data = createProductElement(element);
+    getSection.appendChild(data);
+  });
+};
+getProducts();
+
+const createLoading = () => {
+  const p = document.createElement('p');
+  p.className = 'loading';
+  p.innerText = 'carregando...';
+  return p;
+};
+
+console.log(createLoading());
